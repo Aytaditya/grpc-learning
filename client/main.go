13776e708816e20a7415f7d5eedb1c2b0bbcb35d
Aditya Aryan <adityaaryan531@gmail.com>
2026-01-16 19:54:11 +0530

    client and server

--- client/main.go
+++ client/main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const port = ":8080"
+
+func main() {
+	con, err := grpc.Dial("localhost"+port, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		panic(err)
+	}
+	defer con.Close()
+
+	//client:=pb.NewGreetServiceClient(con)
+
+	// names:=&pb.Namelist{
+	// 	Names:[]string{"Aytaditya","Budi","Citra"},
+	// }
+
+	//callSayHello(client)
+}
--- server/main.go
+++ server/main.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"fmt"
+	"net"
+
+	pb "github.com/Aytaditya/grpc-learning/proto"
+	"google.golang.org/grpc"
+)
+
+const port = ":8080"
+
+type helloServer struct {
+	pb.GreetServiceServer
+}
+
+func main() {
+	ls, err := net.Listen("tcp", port)
+	if err != nil {
+		panic(err)
+	}
+	grpcServer := grpc.NewServer()
+	pb.RegisterGreetServiceServer(grpcServer, &helloServer{})
+	fmt.Println("gRPC server is running on port", ls.Addr())
+	err = grpcServer.Serve(ls)
+	if err != nil {
+		panic(err)
+	}
+}
